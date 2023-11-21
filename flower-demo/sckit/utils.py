59dc52a1609f9500dd48cd52235f510f5556e9a5
Mert <gencturkmert1@gmail.com>
2023-11-21 23:17:41 +0300

    sckit

---
+++ flower-demo/sckit/utils.py
@@ -0,0 +1,76 @@
+from typing import Tuple, Union, List
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import fetch_california_housing
+import openml
+
+XY = Tuple[np.ndarray, np.ndarray]
+Dataset = Tuple[XY, XY]
+LogRegParams = Union[XY, Tuple[np.ndarray]]
+XYList = List[XY]
+
+
+def get_model_parameters(model: LogisticRegression) -> LogRegParams:
+    """Returns the paramters of a sklearn LogisticRegression model."""
+    if model.fit_intercept:
+        params = [
+            model.coef_,
+            model.intercept_,
+        ]
+    else:
+        params = [
+            model.coef_,
+        ]
+    return params
+
+
+def set_model_params(
+    model: LogisticRegression, params: LogRegParams
+) -> LogisticRegression:
+    """Sets the parameters of a sklean LogisticRegression model."""
+    model.coef_ = params[0]
+    if model.fit_intercept:
+        model.intercept_ = params[1]
+    return model
+
+
+def set_initial_params(model: LogisticRegression):
+    """Sets initial parameters as zeros Required since model params are uninitialized
+    until model.fit is called.
+
+    But server asks for initial parameters from clients at launch. Refer to
+    sklearn.linear_model.LogisticRegression documentation for more information.
+    """
+    n_classes = 10  # MNIST has 10 classes
+    n_features = 784  # Number of features in dataset
+    model.classes_ = np.array([i for i in range(10)])
+
+    model.coef_ = np.zeros((n_classes, n_features))
+    if model.fit_intercept:
+        model.intercept_ = np.zeros((n_classes,))
+
+
+def load_mnist() -> Dataset:
+    test_size=0.2
+    random_state=42
+    housing = fetch_california_housing()
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        housing.data, housing.target, test_size=test_size, random_state=random_state
+    )
+
+    return X_train, X_test, y_train, y_test
+
+
+def shuffle(X: np.ndarray, y: np.ndarray) -> XY:
+    """Shuffle X and y."""
+    rng = np.random.default_rng()
+    idx = rng.permutation(len(X))
+    return X[idx], y[idx]
+
+
+def partition(X: np.ndarray, y: np.ndarray, num_partitions: int) -> XYList:
+    """Split X and y into a number of partitions."""
+    return list(
+        zip(np.array_split(X, num_partitions), np.array_split(y, num_partitions))
+    )
--- flower-demo/torch/scaler.py
+++ flower-demo/torch/scaler.py
@@ -9,8 +9,8 @@ class DatasetScaler:
         self.scaler = MinMaxScaler()
 
     def scale_dataset(self, fraction_df):
-        features = fraction_df.drop("target_column", axis=1)
-        labels = fraction_df["target_column"]
+        features = fraction_df.drop("target", axis=1)
+        labels = fraction_df["target"]
 
         scaled_features = (features - self.global_min) / (self.global_max - self.global_min)
         
